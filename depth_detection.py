f958677e49553dd52ea8e8be60fbf29844570ac4
Zeyad <zeyadmoustafa01@gmail.com>
2024-01-25 16:16:02 -0500

    initial commit

---
+++ depth_detection.py
@@ -0,0 +1,47 @@
+import cv2
+import torch
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# Load a MiDas model for depth estimation
+# model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+# model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+# Move model to GPU if available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+# Load transforms to resize and normalize the image
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+img = plt.imread(fname="zeyad.jpg")
+input_batch = transform(img).to(device)
+
+with torch.no_grad():
+        prediction = midas(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+depth_map = prediction.cpu().numpy()
+depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
+depth_map = (depth_map*255).astype(np.uint8)
+depth_map = cv2.applyColorMap(depth_map , cv2.COLORMAP_MAGMA)
+
+cv2.imwrite(filename=f"results/depth_map_{model_type}.png", img=depth_map)
